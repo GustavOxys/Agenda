9f0120271249591f81a224a9297d437f62f8f885
GustavOxys <gustavohzmdev@gmail.com>
2024-05-08 11:26:14 -0300

    Várias alterações em todo o projeto, css, html, views, models e formulários

--- contact/admin.py
+++ contact/admin.py
@@ -1,22 +1,21 @@
 from django.contrib import admin
+
 from contact import models
 
-# Register your models here.
+
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'phone', 'category', 'show')
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
+    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
-    list_max_show_all = 100
-    list_editable = 'phone', 'show'
-    list_display_links = 'first_name',  
-    list_filter = 'id', 'first_name', 'category'
-    
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name', 'show',
+    list_display_links = 'id', 'phone',
 
 
 @admin.register(models.Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = 'name',
     ordering = '-id',
-    
---
+++ contact/views/user_forms.py
@@ -0,0 +1,63 @@
+from django.contrib import messages
+from contact.forms import RegisterForm, RegisterUpdateForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib import auth
+
+
+def register(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Usuário registrado com sucesso')
+            return redirect('contact:index')
+
+    return render(request, 'contact/register.html', {'form' : form})
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'logado com sucesso')
+            return redirect('contact:index')
+        messages.error(request,'Login inválido')
+
+    return render(
+        request,
+        'contact/login.html',
+        {
+            'form': form
+        }
+    )
+
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = RegisterUpdateForm(data=request.POST, instance= request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('contact:user_update')
+            
+
+    return render(
+        request,
+        'contact/update_user.html',
+        {'form' : form}
+    )

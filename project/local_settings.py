cda2163c34d24f094433732e9c162f9fbc4929c1
GustavOxys <gustavohzmdev@gmail.com>
2024-10-09 14:28:26 -0300

    alterado titulo para teste no deploy, agenda > Contacts

--- project/local_settings.py
+++ project/local_settings.py
@@ -1,1 +1,17 @@
-SECRET_KEY = 'senha_forte_qualquer'
+'''SECRET_KEY = '#mDH?Xu_zqT_XpxcJ;etXw,j*=n?[Ek6E$&l6T2^-SQc:&94vJR/EYsm0I8Nf'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['35.193.214.217']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'base_de_dados',
+        'USER': 'meu_usuario',
+        'PASSWORD': 'senha_do_usuario',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}'''
